63533bf61ae4ec777c5efbebd8493176536c1f39
walter.lucuman <walter.lucuman@debian-8-amd64.fi.uncoma.edu.ar>
2025-03-21 10:45:07 -0300

    sss

--- TP01/Pila.java
+++ TP01/Pila.java
@@ -5,10 +5,62 @@ public class Pila {
         this.tope=null;
     }
 
+    public boolean esVacia(){
+        return this.tope == null;
+    }
+
     public boolean apilar(Object tipElem){
         Nodo nuevo = new Nodo(tipElem, tope);
         this.tope=nuevo;
         return true;
+    }
+
+    public boolean desapilar(){
+        boolean b = false;
+
+        //     if(esVacia()){
+        //         b=false;
+        //     }else{
+        //         this.tope=this.tope.getEnlace();
+        //         b=true;
+        //     }
+        // return b;
+
+        if(this.tope !=null){
+            Nodo aux =this.tope.getEnlace();
+            this.tope = aux;
+            b = true;
+        }
+        return b;
+    }
+
+    public Object obtenerTope(){
+        return this.tope == null ? null : this.tope.getElem();
+    }
+
+    public void vaciar(){
+        while(this.tope!=null){
+            this.tope.setElem(null);
+            this.tope=this.tope.getEnlace();
+        }
+        System.out.println("pila vacia");
+    }
+
+    public Pila clone(){
+        Pila copia = new Pila();
+        copia.tope = cloneNodo(this.tope);
+        
+        return copia;
+    }
+
+    public Nodo cloneNodo(Nodo top){
+        Nodo copiaTope=top;
+        if (copiaTope.getEnlace()==null) {
+            copiaTope=new Nodo(top.getElem(),null);
+        } else {
+            copiaTope=new Nodo(top.getElem(), cloneNodo(copiaTope.getEnlace()));
+        }
+        return copiaTope;
     }
 
     @Override
@@ -25,9 +77,9 @@ public class Pila {
                 s+=aux.getElem().toString();
                 aux= aux.getEnlace();
                 if(aux !=null){
-                    s+=")";
+                    s+=",";
                 }
-                s+=",";
+
             }
             s+="]";
         }
