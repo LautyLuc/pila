9e480f40bb0fd9cea8fef23b87b8b0818d2a719a
walter.lucuman <walter.lucuman@debian-8-amd64.fi.uncoma.edu.ar>
2025-03-28 11:38:48 -0300

    no se

--- linealesDinamicas/Cola.java
+++ linealesDinamicas/Cola.java
@@ -50,30 +50,48 @@ public class Cola {
         }
     }
 
-    public Cola clone() {
+    // public Cola clone() {
+    //     Cola colacha = new Cola();
+    //     if (this.frente != null) {
+    //         colacha.frente = cloneNodos(this.frente);
+    //         Nodo aux = colacha.frente;
+    //         while (aux.getEnlace() != null) {
+    //             aux = aux.getEnlace();
+    //         }
+    //         colacha.fin = aux;
+    //     }
+    //     return colacha;
+    // }
+
+    // public Nodo cloneNodos(Nodo n1) {
+    //     Nodo nodito = null;
+    //     if (n1 != null) {
+    //         nodito = new Nodo(n1.getElem(), null);
+    //         if (n1.getEnlace() != null) {
+    //             nodito.setEnlace(cloneNodos(n1.getEnlace()));
+    //         }
+    //     }
+    //     return nodito;
+    // }
+    
+    public Cola clone(){
         Cola colacha = new Cola();
-        if (this.frente != null) {
-            colacha.frente = cloneNodos(this.frente);
-            Nodo aux = colacha.frente;
-            while (aux.getEnlace() != null) {
-                aux = aux.getEnlace();
-            }
-            colacha.fin = aux;
+        Nodo aux=this.frente;
+        Nodo nodito= new Nodo(aux.getElem(),null);
+
+        colacha.frente=nodito;
+        colacha.fin= nodito;
+        while (aux!=null) {
+            aux=aux.getEnlace();
+            nodito= new Nodo(aux.getElem(),null);
+            
         }
+
+
+
         return colacha;
     }
 
-    public Nodo cloneNodos(Nodo n1) {
-        Nodo nodito = null;
-        if (n1 != null) {
-            nodito = new Nodo(n1.getElem(), null);
-            if (n1.getEnlace() != null) {
-                nodito.setEnlace(cloneNodos(n1.getEnlace()));
-            }
-        }
-        return nodito;
-    }
-    
     @Override
     public String toString(){
         String s="";
